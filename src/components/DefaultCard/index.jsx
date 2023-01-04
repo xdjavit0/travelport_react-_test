81e4be6788648a90e0ccaedff8ff376c9989d796
xdjavit0 <javier.carrascosa@sarria.salesians.cat>
2023-01-04 09:09:47 +0100

    little refactor

--- src/components/DefaultCard/index.jsx
+++ src/components/DefaultCard/index.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { Card, Email, Name, Picture } from '../DefaultCard/styles.js'
 
 const DefaultCard = ({ name, surname, photo, email }) => {
-    return (
-      <Card>
-        <Picture src={photo} alt='Not found'/>
-        <Name>{name} {surname}</Name>
-        <Email>{email}</Email>
-      </Card>
-    )
-  }
-  export default DefaultCard
+  return (
+    <Card>
+      <Picture src={photo} alt='Not found'/>
+      <Name>{name} {surname}</Name>
+      <Email>{email}</Email>
+    </Card>
+  )
+}
+export default DefaultCard
