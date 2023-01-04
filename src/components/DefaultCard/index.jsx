423fe94ece602bc3ca44be50d2e5bcbb23fe1863
xdjavit0 <javier.carrascosa@sarria.salesians.cat>
2023-01-04 11:10:03 +0100

    classnames

--- src/components/DefaultCard/index.jsx
+++ src/components/DefaultCard/index.jsx
@@ -3,10 +3,10 @@ import { Card, Email, Name, Picture } from '../DefaultCard/styles.js'
 
 const DefaultCard = ({ name, surname, photo, email }) => {
     return (
-      <Card>
-        <Picture src={photo} alt='Not found'/>
-        <Name>{name} {surname}</Name>
-        <Email>{email}</Email>
+      <Card className='CardFrame' data-testid='CardContainer'>
+        <Picture className='CardPicture' data-testid='CardPhoto' src={photo} alt='Not found'/>
+        <Name className='CardNameSurname' data-testid='CardName'>{name} {surname}</Name>
+        <Email className='CardContact' data-testid='CardEmail'>{email}</Email>
       </Card>
     )
   }
